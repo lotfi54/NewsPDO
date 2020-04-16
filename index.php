cbad3da15dde44b6e99150727a5370ddaaf3f221
lotfi54 <lotfibouraine@gmail.com>
2020-04-16 10:53:46 +0200

    modification de autoload

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
 require 'lib/autoload.php'; 
-$bdd = DBfactory::getMysqlConnexionWhitPDO(); 
+$bdd = DBFactory::getMysqlConnexionWhitPDO(); 
 $manager = new NewsManagerPDO($bdd); 
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>News</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body>
+<h1>Hello</h1>
+</body>
+</html>
